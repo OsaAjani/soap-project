996c7d9c8f96f7faba68139ba4e5e86a0d112cd6
Eliott Bureau <eliottbureau@gmail.com>
2016-01-15 16:47:55 +0100

    check static truck

--- controllers/internalConsole.php
+++ controllers/internalConsole.php
@@ -2,21 +2,38 @@
 	/**
 	 * Cette page gère les appels de la console
 	 */
+	use PhpAmqpLib\Connection\AMQPStreamConnection;
+	use PhpAmqpLib\Message\AMQPMessage;
+
+
 	class internalConsole extends Controller
 	{
-		public function checkRunTruck ()
+		public function checkStaticTruck () 
 		{
-			public function checkStaticTruck () 
+			global $db;
+			
+			$pathsInProgress = $db->getFromTableWhere('path', ['status' => internalConstants::$pathStatus['RUN']]);
+			
+			if (count($pathsInProgress))
 			{
-				//Select tout les truck en cours
-				global $db;
-				
-				$pathsInProgress = $db->getFromTableWhere('path', ['status' => internalConstants::$pathStatus['RUN']])
-				$truckRun = array();
+				$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+				$channel = $connection->channel();
 
 				foreach ($pathsInProgress as $path) {
-					$truckRun[] = $path['']
+					$channel->queue_declare('check_truck', false, true, false, false);
+					$data = array(
+						'path_id' 	=> $path['id'],
+						'truck'		=> $path['truck_id']
+					);
+					
+					$msg = new AMQPMessage(json_encode($data),
+			        	array('delivery_mode' => 2) # make message persistent
+			        );
+						$channel->basic_publish($msg, '', 'check_truck');
 				}
-			}
+
+				$channel->close();
+				$connection->close();
+			}	
 		}
 	}
--- controllers/internalWorkers.php
+++ controllers/internalWorkers.php
@@ -4,7 +4,7 @@
 
 	class internalWorkers extends Controller
 	{
-		public function _workerStatus ()
+		public function _status ()
 		{
 			$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 			$channel = $connection->channel();
@@ -14,6 +14,9 @@
 			$callback = function($msg){
 				global $db;
 				$params = json_decode($msg->body, true);
+
+				//IF STATUS == 3 (DOWN) SEND SMS TO REPAIRER
+
 				$db->updateTableWhere('path', ['status' => $params['status']], ['id' => $params['path_id']]);
 	   			$msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
 			};
@@ -29,7 +32,7 @@
 			$connection->close();
 		}
 
-		public function _workerPosition ()
+		public function _position ()
 		{
 			$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 			$channel = $connection->channel();
@@ -54,8 +57,46 @@
 			$connection->close();
 		}
 
-		public function _workerCheckTruck () 
+		public function _checkTruck () 
 		{
+			$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+			$channel = $connection->channel();
 
+			$channel->queue_declare('check_truck', false, true, false, false);
+
+			$callback = function($msg){
+				global $db;
+				$path = json_decode($msg->body, true);
+				$now = new \DateTime();
+				$now->sub(new \DateInterval('PT5M'));
+				$positions = $db->getFromTableWhere('position', ['>at' => $now->format('Y-m-d H:i:s')]);
+				$static = true;
+				$previousLongitude = $positions[0]['longitude'];
+				$previousLatitude = $positions[0]['latitude'];
+				foreach ($positions as $position) 
+				{
+					if ($position['longitude'] != $previousLongitude || $position['latitude'] != $previousLatitude)
+					{
+						$static = false;
+						break;
+					}
+				}
+				if ($static == true)
+				{
+					// ENVOYER SMS CONDUCTEUR
+					echo 'static';
+				}
+	   			$msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+			};
+
+			$channel->basic_qos(null, 1, null);
+			$channel->basic_consume('check_truck', '', false, false, false, false, $callback);
+
+			while(count($channel->callbacks)) {
+	    		$channel->wait();
+			}
+
+			$channel->close();
+			$connection->close();
 		}
 	}
