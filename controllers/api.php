ad16df21a1317de5dd69210d126d18eedb333174
Eliott Bureau <eliottbureau@gmail.com>
2016-01-15 13:54:56 +0100

    Api GET path OK

--- controllers/api.php
+++ controllers/api.php
@@ -2,15 +2,50 @@
 /**
  * API Rest
  */
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
 class api extends ApiController
 {
 	public function putStatus ($idPath, $status)
 	{
-		return $this->autoHttpCode();
+		$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+		$channel = $connection->channel();
+
+		$channel->queue_declare('change_status', false, true, false, false);
+		//send to Queue info change status
+
+		$channel->close();
+		$connection->close();
+		return $this->autoHttpCode(true);
 	}
 
 	public function postPostion ($idPath, $latitude, $longitude, $at)
 	{
 		return $this->autoHttpCode();
 	}
+
+	public function getPath ($idPath)
+	{
+		global $db;
+
+		if(!$path = $db->getFromTableWhere('path', ['id' => $idPath]))
+		{
+			return $this->autoHttpCode(false);
+		}
+
+		$path = $path[0];
+		
+		$driver = $db->getFromTableWhere('driver', ['id' => $path["driver_id"]]);
+		$truck = $db->getFromTableWhere('truck', ['id' => $path["truck_id"]]);
+		
+		$path["driver"] = $driver[0];
+		$path["truck"] = $truck[0];
+		
+		unset($path["driver_id"]);
+		unset($path["truck_id"]);
+
+		return $this->json($path)->autoHttpCode();
+	}
+
 }
