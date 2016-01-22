f4cf17a979a40924e002f188af00fd699d3c56d5
root <root@ajani.hd.free.fr>
2016-01-22 12:09:44 +0100

    Ajout du script qui lance les workers

---
+++ startWorkers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+/var/www/soap-project/console.php internalWorkers status
+/var/www/soap-project/console.php internalWorkers status
+
+/var/www/soap-project/console.php internalWorkers position
+/var/www/soap-project/console.php internalWorkers position
+
+/var/www/soap-project/console.php internalWorkers checkTruck
+/var/www/soap-project/console.php internalWorkers checkTruck
+
+/var/www/soap-project/console.php internalWorkers checkIntervention
+/var/www/soap-project/console.php internalWorkers checkIntervention
