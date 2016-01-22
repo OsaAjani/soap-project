722bdc18dbff6d3e853991bc5bdb84e393da2fc8
root <root@ajani.hd.free.fr>
2016-01-22 13:25:31 +0100

    Fix espace insert model, tache fond startWorkers.sh

--- startWorkers.sh
+++ startWorkers.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-/var/www/soap-project/console.php internalWorkers status
-/var/www/soap-project/console.php internalWorkers status
+/var/www/soap-project/console.php internalWorkers status > /dev/null 2>&1 &
+/var/www/soap-project/console.php internalWorkers status > /dev/null 2>&1 &
 
-/var/www/soap-project/console.php internalWorkers position
-/var/www/soap-project/console.php internalWorkers position
+/var/www/soap-project/console.php internalWorkers position > /dev/null 2>&1 &
+/var/www/soap-project/console.php internalWorkers position > /dev/null 2>&1 &
 
-/var/www/soap-project/console.php internalWorkers checkTruck
-/var/www/soap-project/console.php internalWorkers checkTruck
+/var/www/soap-project/console.php internalWorkers checkTruck > /dev/null 2>&1 &
+/var/www/soap-project/console.php internalWorkers checkTruck > /dev/null 2>&1 &
 
-/var/www/soap-project/console.php internalWorkers checkIntervention
-/var/www/soap-project/console.php internalWorkers checkIntervention
+/var/www/soap-project/console.php internalWorkers checkIntervention > /dev/null 2>&1 &
+/var/www/soap-project/console.php internalWorkers checkIntervention > /dev/null 2>&1 &
