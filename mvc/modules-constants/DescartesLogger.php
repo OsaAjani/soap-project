afa0dacc6df683317b9065fc3d7664b0d02cd1a3
Eliott Bureau <eliottbureau@gmail.com>
2016-01-22 17:11:54 +0100

    fix logger

--- mvc/modules-constants/DescartesLogger.php
+++ mvc/modules-constants/DescartesLogger.php
@@ -4,6 +4,6 @@
 	define('DESCARTESLOGGER_MIN_LEVEL', 0); //On active les logs à partir du niveau debug
 	define('DESCARTESLOGGER_EMAIL', false); //On ne défini pas d'email pour le logger et on le desactive
 	define('DESCARTESLOGGER_MIN_LEVEL_EMAIL', 4); //On n'active l'envoi d'email qu'à partir du niveau ERROR
-	define('DESCARTESLOGGER_FILE', 'php://stderr'); //On défini le fichier de log à employer
+	define('DESCARTESLOGGER_FILE', PWD . 'soap-project.log'); //On défini le fichier de log à employer
 
 
